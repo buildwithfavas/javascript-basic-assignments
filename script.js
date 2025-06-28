29cce4c0f22615071d3b346acd4fa4caa5a58fae
Mohammed Favas <mohammedfavas888@gmail.com>
2025-06-28 13:05:55 +0530

    Added some more assignments and question as doc

--- script.js
+++ script.js
@@ -311,3 +311,308 @@ const prompt = require('prompt-sync')();
 
 // main();
 
+//18
+// const writtenTest = parseFloat(prompt("Enter the marks for Written test: "));
+// const labExams = parseFloat(prompt("Enter the marks for Lab exams: "));
+// const assignments = parseFloat(prompt("Enter the marks for Assignments: "));
+// const grade = (writtenTest * 0.70) + (labExams * 0.20) + (assignments * 0.10);
+
+// console.log("Grade of the student is: " + grade.toFixed(1));
+
+//19
+// const income = parseFloat(prompt("Enter the annual income :"));
+
+// let tax = 0;
+
+// if (income <= 250000) {
+//   tax = 0;
+// } else if (income <= 500000) {
+//   tax = income * 0.05;
+// } else if (income <= 1000000) {
+//   tax = income * 0.20;
+// } else if (income <= 5000000) {
+//   tax = income * 0.30;
+// }
+
+// console.log("Income tax amount = " + tax.toFixed(2));
+
+//20
+// let k = 1;
+// for(let i = 1;i <= 4;i++){
+//     let text = "";
+//     for(let j = 1;j <= i;j++){
+//         text += k + " "; 
+//         k++;
+//     }
+//     console.log(text);
+// }
+
+//21
+// const limit = parseInt(prompt("Enter the array limit: "));
+// let arr = [];
+
+// for (let i = 0; i < limit; i++) {
+//   arr[i] = parseInt(prompt("Enter element " + (i + 1)+": "));
+// }
+
+// let result = [];
+// for (let i = 0; i < arr.length - 1; i++) {
+//   result[i] = arr[i] * arr[i + 1];
+// }
+
+// console.log("Output: ");
+// console.log(result.join("\t"));
+
+//22
+// // Declare arrays globally so all functions can access
+// let array1 = [];
+// let array2 = [];
+// let sumArray = [];
+// let size = 0;
+
+// // Function to get arrays
+// function getArray() {
+//   size = parseInt(prompt("Enter the size of array"));
+//   // Get Array 1
+//   console.log("Enter the values of array 1");
+//   for (let i = 0; i < size; i++) {
+//     array1[i] = [];
+//     for (let j = 0; j < size; j++) {
+//       array1[i][j] = parseInt(prompt(`Array 1 [${i}][${j}]:`));
+//     }
+//   }
+//   // Get Array 2
+//   console.log("Enter the values of array 2");
+//   for (let i = 0; i < size; i++) {
+//     array2[i] = [];
+//     for (let j = 0; j < size; j++) {
+//       array2[i][j] = parseInt(prompt(`Array 2 [${i}][${j}]:`));
+//     }
+//   }
+// }
+// // Function to add arrays
+// function addArray() {
+//   for (let i = 0; i < size; i++) {
+//     sumArray[i] = [];
+//     for (let j = 0; j < size; j++) {
+//       sumArray[i][j] = array1[i][j] + array2[i][j];
+//     }
+//   }
+// }
+// // Function to display result
+// function displayArray() {
+//   console.log("Sum of array 1 and array 2:");
+//   for (let i = 0; i < size; i++) {
+//     let row = "";
+//     for (let j = 0; j < size; j++) {
+//       row += sumArray[i][j] + "\t";
+//     }
+//     console.log(row);
+//   }
+// }
+// // Main function
+// function main() {
+//   getArray();
+//   addArray();
+//   displayArray();
+// }
+// // Run the main function
+// main();
+
+//23
+// // Define Matrix class
+// class Matrix {
+//   constructor() {
+//     this.size = 0;
+//     this.array = [];
+//   }
+//   // Method to get array values
+//   getArray() {
+//     this.size = parseInt(prompt("Enter the size of array: "));
+//     console.log("Enter the array values: ");
+//     for (let i = 0; i < this.size; i++) {
+//       this.array[i] = [];
+//       for (let j = 0; j < this.size; j++) {
+//         this.array[i][j] = parseInt(prompt(`Element [${i}][${j}]: `));
+//       }
+//     }
+//   }
+//   // Method to display array values
+//   displayArray() {
+//     console.log("Array elements are: ");
+//     for (let i = 0; i < this.size; i++) {
+//       let row = "";
+//       for (let j = 0; j < this.size; j++) {
+//         row += this.array[i][j] + "\t";
+//       }
+//       console.log(row);
+//     }
+//   }
+// }
+// // Main function
+// function main() {
+//   const matrix = new Matrix();
+//   matrix.getArray();
+//   matrix.displayArray();
+// }
+// // Run main
+// main();
+
+//24
+// // Parent class Area
+// class Area {
+//   circle(radius) {
+//     const area = 3.14 * radius * radius;
+//     console.log(`Area of the circle is: ${area.toFixed(2)}`);
+//   }
+
+//   square(length) {
+//     const area = length * length;
+//     console.log(`Area of the square is: ${area.toFixed(2)}`);
+//   }
+
+//   rectangle(length, width) {
+//     const area = length * width;
+//     console.log(`Area of the rectangle is: ${area.toFixed(2)}`);
+//   }
+
+//   triangle(base, height) {
+//     const area = 0.5 * base * height;
+//     console.log(`Area of the triangle is: ${area.toFixed(2)}`);
+//   }
+// }
+
+// // Child class MyClass extends Area
+// class MyClass extends Area {
+//   main() {
+//     console.log("");
+//     const choice = prompt("Select a choice from below list: \n1. Circle\n2. Square\n3. Rectangle\n4. Triangle\nEnter your choice: ");
+
+//     switch (choice) {
+//       case "1":
+//         const radius = parseFloat(prompt("Enter the radius:"));
+//         this.circle(radius);
+//         break;
+
+//       case "2":
+//         const length = parseFloat(prompt("Enter the length:"));
+//         this.square(length);
+//         break;
+
+//       case "3":
+//         const rectLength = parseFloat(prompt("Enter the length:"));
+//         const rectWidth = parseFloat(prompt("Enter the width:"));
+//         this.rectangle(rectLength, rectWidth);
+//         break;
+
+//       case "4":
+//         const base = parseFloat(prompt("Enter the base:"));
+//         const height = parseFloat(prompt("Enter the height:"));
+//         this.triangle(base, height);
+//         break;
+
+//       default:
+//         console.log("Invalid choice!");
+//     }
+//   }
+// }
+
+// // Create object and run the main function
+// const obj = new MyClass();
+// obj.main();
+
+//25
+// var library = [ 
+//     {
+//         title: 'The Road Ahead',
+//         author: 'Bill Gates',
+//         readingStatus: true
+//     },
+//     {
+//         title: 'Walter Isaacson',
+//         author: 'Steve Jobs',
+//         readingStatus: true
+//     },
+//     {
+//         title: 'Mockingjay: The Final Book of The Hunger Games',
+//         author: 'Suzanne Collins',
+//         readingStatus: false
+//     }
+// ];
+
+// for (let i = 0; i < library.length; i++) {
+//     const book = library[i];
+//     if (book.readingStatus) {
+//         console.log(`Already read '${book.title}' by ${book.author}.`);
+//     } else {
+//         console.log(`You still need to read '${book.title}' by ${book.author}.`);
+//     }
+// }
+
+//26
+// //let my_string = "1234";
+// //let my_string = Number(123);
+
+// try {
+//   const reversed = my_string.split('').reverse().join('');
+//   console.log(`Reversed string is : ${reversed}`);
+// } catch(err) {
+//   console.log(`Error : ${err.message}`);
+// } finally {
+//   console.log(`Type of my string is : ${typeof my_string}`);
+// }
+
+//27
+// // Example inputs:
+// // let my_height = "seven"; // Sample Input 0
+// // let my_height = 77;      // Sample Input 1
+// // let my_height = 0;       // Sample Input 2
+// // let my_height = 8;       // Sample Input 3
+
+// try {
+//     if (isNaN(my_height)) {
+//         throw new Error("notANumberError");
+//     }
+//     if (my_height > 50) {
+//         throw new Error("hugeHeightError");
+//     }
+//     if (my_height < 5) {
+//         throw new Error("tinyHeightError");
+//     }
+//     console.log(my_height);
+// } catch (err) {
+//     console.log(err.message);
+// }
+
+//28
+// function Car(name, mileage, max_speed) {
+//   this.name = name;
+//   this.mileage = mileage;
+//   this.max_speed = max_speed;
+// }
+
+// let myCar = new Car("Toyota Corolla", 18, 180);
+
+// console.log(myCar.name);
+// console.log(myCar.mileage);
+// console.log(myCar.max_speed);
+
+//29
+function myFilter(myArray, callback) {
+  let sum = 0;
+  for(let i = 0; i < myArray.length; i++) {
+    sum += myArray[i];
+  }
+  const isEven = callback(sum);
+  console.log(`Sum is ${sum} (${isEven ? "even" : "odd"})`);
+  return sum;
+}
+
+function isSumEven(sum) {
+  return sum % 2 === 0;
+}
+
+let num = "1234";
+let digits = num.split("").map(Number);
+let result = myFilter(digits, isSumEven);
+console.log(result);
